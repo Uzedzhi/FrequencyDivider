855efa5f9272cb624e350ccfaff8b854f7a996cb
Uzedzhi <afdimval@gmail.com>
2026-07-06 14:36:27 +0300

    added documentation

--- FrequencyDivider6.v
+++ FrequencyDivider6.v
@@ -1,8 +1,13 @@
+// ==============Делитель частоты на 6============================
+// IN:          clk_in  - входной меандр
+// OUT:         clk_out - меандр с частотой в шесть раз меньше
+// ===============================================================
 module FrequencyDivider6 (
     input clk_in,
     input reset,
     output reg clk_out
 );
+    // внутреннее состояние (3 состояния)
     reg [1:0] s;
 
     always @(posedge clk_in) begin
@@ -10,12 +15,12 @@ module FrequencyDivider6 (
             s <= 0;
             clk_out <= 0;
         end
-        else begin
-            if (s == 2) begin
+        else begin 
+            if (s == 2) begin // если мы прождали 3 такта, то меняем состояние на противоположное
                 s <= 0;
                 clk_out <= ~clk_out;
             end
-            else begin
+            else begin // иначе оставляем то же состояние
                 s <= s + 1;
                 clk_out <= clk_out;
             end
